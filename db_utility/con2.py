759e5643bb39e44118017e31279ba6059d0f4d7f
IDBIADITYA <38806965+IDBIADITYA@users.noreply.github.com>
2021-04-13 15:50:24 +0530

    Adityacomit

--- db_utility/con2.py
+++ db_utility/con2.py
@@ -1,14 +1,14 @@
 import cx_Oracle
-ip = '10.160.33.11'
-port = 1521
-SID = 'sysadm.PS_HRS_APP_RES'
-dsn_tns = cx_Oracle.makedsn(ip, port, SID)
+#ip = '10.160.33.11'
+#port = 1521
+#SID = 'sysadm.PS_HRS_APP_RES'
+#dsn_tns = cx_Oracle.makedsn(ip, port, SID)
 
 # You might get these via environment variable to make thing secure
 username = 'staqo'
 password = 'STAQO123'
 
-conn = cx_Oracle.connect(username, password, dsn_tns)
+conn = cx_Oracle.connect(username, password, "10.160.33.11:1521/sysadm")
 
 print(conn.version)
 conn.close()
